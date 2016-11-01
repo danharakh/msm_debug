6ed7b8926c388821f387f8953348fce834a99962
Dan <danh@users.noreply.github.com>
2016-11-01 18:46:59 -0500

    v1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,12 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find_by({:id =>params["id"]})
+    render("movies/show.html.erb")
   end
 
   def new_form
+    render("movies/mew_form.html.erb")
   end
 
   def create_row
@@ -24,23 +26,24 @@ class MoviesController < ApplicationController
   end
 
   def edit_form
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find_by({:id=>params["id"]})
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie = Movie.find_by({:id=>params["id"]})
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
 
     render("show")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
 
-    movie.destroy
+    @movie.destroy
   end
 end
